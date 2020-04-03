7ad82a6adfcbb88436231412aa2c39c046af1db6
Nain Acero <a_nacerom@unjbg.edu.com>
2020-04-02 21:25:35 -0500

    Menu items logout and Mapa Cliente

--- app/src/main/java/com/nain/cloneuber/activities/client/MapClientActivity.java
+++ app/src/main/java/com/nain/cloneuber/activities/client/MapClientActivity.java
@@ -1,37 +1,71 @@
 package com.nain.cloneuber.activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.nain.cloneuber.R;
 import com.nain.cloneuber.activities.MainActivity;
+import com.nain.cloneuber.includes.MyToolbar;
 import com.nain.cloneuber.providers.AuthProvider;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button mButtonLogout;
-    AuthProvider mAtuchProvider;
+    private AuthProvider mAtuchProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
+        MyToolbar.show(this, "Conductor", false); // Toolbar pra el app instanciada de clase
 
-        mButtonLogout = findViewById(R.id.btnLogout);
         mAtuchProvider = new AuthProvider();
 
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAtuchProvider.logout();
-                Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // para finalizar esta actividad
-            }
-        });
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this); //  es ese fragmento cargamos el mapa de google
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // habilitar el api de google
+        mMap = googleMap;
+        // tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+    // sobreescribir metodo
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.client_menu, menu); // cargamos el menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // sobreescribir metodo
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // verificamos el click sobre los items
+        if(item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAtuchProvider.logout();
+        Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // para finalizar esta actividad
     }
 }
--- app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
@@ -1,13 +1,21 @@
 package com.nain.cloneuber.activities.driver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.nain.cloneuber.R;
+import com.nain.cloneuber.activities.MainActivity;
+import com.nain.cloneuber.activities.client.MapClientActivity;
+import com.nain.cloneuber.includes.MyToolbar;
+import com.nain.cloneuber.providers.AuthProvider;
 
 // interfaz para mapas
 public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -15,14 +23,18 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
 
+    private AuthProvider mAtuchProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+        MyToolbar.show(this, "Conductor", false); // Toolbar pra el app instanciada de clase
 
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        //  es ese fragmento cargamos el mapa de google
-        mMapFragment.getMapAsync(this);
+        mMapFragment.getMapAsync(this); //  es ese fragmento cargamos el mapa de google
+
+        mAtuchProvider = new AuthProvider();
     }
 
     @Override
@@ -31,5 +43,29 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         // tipo de mapa
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    // sobreescribir metodo
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu); // cargamos el menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // sobreescribir metodo
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // verificamos el click sobre los items
+        if(item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAtuchProvider.logout();
+        Intent intent = new Intent(MapDriverActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // para finalizar esta actividad
     }
 }
