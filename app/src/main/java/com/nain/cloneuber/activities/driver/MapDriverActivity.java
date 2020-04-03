d05ba12724d96ca3b94796d956511676d7a7d0bf
Nain Acero <a_nacerom@unjbg.edu.com>
2020-04-03 11:17:09 -0500

    Ubicación com Permisos

--- app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
@@ -1,16 +1,35 @@
 package com.nain.cloneuber.activities.driver;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.nain.cloneuber.R;
 import com.nain.cloneuber.activities.MainActivity;
 import com.nain.cloneuber.activities.client.MapClientActivity;
@@ -25,11 +44,41 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
 
     private AuthProvider mAtuchProvider;
 
+    // propiedades del gps
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation; // para poder iniciar o detener la ubicación
+    private final static int LOCATION_REQUEST_CODE = 1; // saber si deberia solicitar permisos o no del gps
+
+    //escuchara cada vez que el usuario se mueva
+    LocationCallback mLocationCallback = new LocationCallback() {
+        // sebreescribimos un método
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            // contexto de la aplicación
+            for(Location location: locationResult.getLocations()) {
+                if(getApplicationContext() != null) {
+                    // obtenemos la localización del usuario en tiempo real
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    // posición actual
+                                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                    .zoom(15f)
+                                    .build()
+                    ));
+                }
+            }
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
         MyToolbar.show(this, "Conductor", false); // Toolbar pra el app instanciada de clase
+
+        // instanciamos el mFusedLocation
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this); // para poder iniciar o detener la ubicación
 
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this); //  es ese fragmento cargamos el mapa de google
@@ -43,6 +92,87 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         // tipo de mapa
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true); // para mostrar el zoom
+
+        // instaciamos el gps
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000); // tiempo en que se estara actualizando la ubicación del usuario
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // para que use el gps con la mayor presión
+        mLocationRequest.setSmallestDisplacement(5);
+
+        startLocation();
+    }
+
+    // permisos de la aplicación
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_REQUEST_CODE) {
+            // preguntar si consedio los permisos
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // preguntar si consedio los permisos
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    // obtenemos la ubicación actualizada y en tiempo real
+                    mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()); // Looper.myLooper() => devuelve el hilo actual
+                } else {
+                    checkLocationPermission();
+                }
+            } else {
+                checkLocationPermission();
+            }
+        }
+    }
+
+    // metodo para el escuchador de nuestra aplicación => lo ejecutamos en el onMapReady
+    private void startLocation(){
+        // verificamos que la version de android sea mayor a machMelon
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // preguntar si los permisson ya estan consedidos
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                // obtenemos la ubicación actualizada y en tiempo real
+                mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()); // Looper.myLooper() => devuelve el hilo actual
+            }else {
+                // si no estan los permisos consedidos llamamos al Dialog
+                checkLocationPermission();
+            }
+
+        }else {
+            // ejecutamos defrente se supone que consedio cuando instalo el app , android inferior
+            mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        }
+
+    }
+
+    // metodo pra validar en caso de que el usuario no acepte los permisos
+    private void checkLocationPermission(){
+        // preguntar si el usuario no consedio los permisos
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // si denegaste permissos
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // creamos una alerta
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.txt_error_location)
+                        .setMessage(R.string.txt_message_permission)
+                        // creamos un boton con un eventLisener
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // habilitamos los permisos para poder utilizarlo en el celular
+                                ActivityCompat.requestPermissions(MapDriverActivity.this, new String[] {
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                }, LOCATION_REQUEST_CODE );
+                            }
+                        })
+                .create()
+                .show();
+            } else {
+                // habilitamos los permisos para poder utilizarlo en el celular
+                ActivityCompat.requestPermissions(MapDriverActivity.this, new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                }, LOCATION_REQUEST_CODE );
+            }
+        }
     }
 
     // sobreescribir metodo
