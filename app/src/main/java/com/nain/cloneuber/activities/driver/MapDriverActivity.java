c0fb7a7df6fb84697d6eaf91f8333433e1781482
Nain Acero <a_nacerom@unjbg.edu.com>
2020-04-02 20:49:40 -0500

    cargar Mapa

--- app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/nain/cloneuber/activities/driver/MapDriverActivity.java
@@ -4,13 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.nain.cloneuber.R;
 
-public class MapDriverActivity extends AppCompatActivity {
+// interfaz para mapas
+public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //  es ese fragmento cargamos el mapa de google
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // habilitar el api de google
+        mMap = googleMap;
+        // tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
