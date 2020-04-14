9b8650f1ad1b8bf6c43078dd7868fbe623104502
Nain Acero <a_nacerom@unjbg.edu.com>
2020-04-13 21:13:22 -0500

    Important =>  Counter de 10 segundo temporizador  Sonido de Notificacion y ciclo de vida en android

--- app/src/main/java/com/nain/cloneuber/activities/driver/NotificationBookingActivity.java
+++ app/src/main/java/com/nain/cloneuber/activities/driver/NotificationBookingActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,7 +21,7 @@ import com.nain.cloneuber.providers.GeoFireProvider;
 
 public class NotificationBookingActivity extends AppCompatActivity {
 
-    private TextView mTextViewDestination,mTextViewOrigin,mTextViewMin,mTextViewDistance;
+    private TextView mTextViewDestination,mTextViewOrigin,mTextViewMin,mTextViewDistance,mTextViewCounter;
     private Button mButtonAccept,mButtonCancel;
 
     private ClientBookingProvider mclientBookingProvider;
@@ -33,6 +35,29 @@ public class NotificationBookingActivity extends AppCompatActivity {
     private String mExtraMin;
     private String mExtraDistance;
 
+    private Handler mhandler; // para el contador
+    private int mcounter = 10;
+    Runnable rounable = new Runnable() {
+        @Override
+        public void run() {
+            mcounter = mcounter -1;
+            mTextViewCounter.setText(String.valueOf(mcounter));
+            if(mcounter > 0) {
+                initTimer();
+            }else {
+                cancelBooking(); // cancelamos el viaje
+            }
+        }
+    };
+
+    private MediaPlayer mediaPlayer;
+
+    // para inicializar el contador
+    private void initTimer() {
+        mhandler = new Handler();
+        mhandler.postDelayed(rounable, 1000);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +69,7 @@ public class NotificationBookingActivity extends AppCompatActivity {
         mTextViewDistance = findViewById(R.id.textViewDistance);
         mButtonAccept = findViewById(R.id.btnAcceptBooking);
         mButtonCancel = findViewById(R.id.btnCancelBooking);
+        mTextViewCounter = findViewById(R.id.textViewCounter);
 
         mExtraIdClient = getIntent().getStringExtra("idClient");
         mExtraOrigin = getIntent().getStringExtra("origin");
@@ -56,6 +82,9 @@ public class NotificationBookingActivity extends AppCompatActivity {
         mTextViewMin.setText(mExtraMin);
         mTextViewDistance.setText(mExtraDistance);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.ringtone);
+        mediaPlayer.setLooping(true); // para que el sonido se repita varias veces
+
         // PARA ENCENDER EL CELULAR CUANDO ESTE APAGADO PARA MOSTRAR ESTA ACTIVIDAD
         getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
@@ -63,6 +92,8 @@ public class NotificationBookingActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
         );
+
+        initTimer();
 
         mButtonAccept.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,7 +111,10 @@ public class NotificationBookingActivity extends AppCompatActivity {
     }
 
     private void cancelBooking() {
-
+        if(mhandler != null) {
+            // cuando le damos en cancelar nos aseguramos de que el hander deje de contar
+            mhandler.removeCallbacks(rounable);
+        }
         mclientBookingProvider = new ClientBookingProvider();
         mclientBookingProvider.updateStatus(mExtraIdClient, "cancel");
 
@@ -93,6 +127,10 @@ public class NotificationBookingActivity extends AppCompatActivity {
     }
 
     private void acceptBooking() {
+        if(mhandler != null) {
+            // cuando le damos en cancelar nos aseguramos de que el hander deje de contar
+            mhandler.removeCallbacks(rounable);
+        }
         // lo eliminamos de fireabse database
         mAuthProvider = new AuthProvider();
         geoFireProvider = new GeoFireProvider("active_drivers");
@@ -111,5 +149,56 @@ public class NotificationBookingActivity extends AppCompatActivity {
         intent1.setAction(Intent.ACTION_RUN);
         intent1.putExtra("idClient", mExtraIdClient); // pasamos como un parametro extra el id del cliente a la actividad MapDriverBookingActivity
         startActivity(intent1);
+    }
+
+    // otro ciclo de vida se ejecuta cuando minimizamos la aplicacion
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer != null) {
+            if(mediaPlayer.isPlaying()) {
+                // verificamos que no este sonando
+                mediaPlayer.release();
+            }
+        }
+    }
+
+    //cuando el usuario abandona la actividad
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mediaPlayer != null) {
+            if(mediaPlayer.isPlaying()){
+                // verificar si esta sonando
+                mediaPlayer.pause();
+            }
+        }
+    }
+
+    // metodo de ciclo de vida => se ejecuta cuando la actividad a sido creada
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mediaPlayer != null) {
+            if(!mediaPlayer.isPlaying()){
+                // verificar si esta sonando
+                mediaPlayer.start();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mhandler != null) {
+            // cuando le damos en cancelar nos aseguramos de que el hander deje de contar
+            mhandler.removeCallbacks(rounable);
+        }
+        if(mediaPlayer != null) {
+            if(mediaPlayer.isPlaying()){
+                // verificar si esta sonando
+                mediaPlayer.pause(); // dejar de sonar
+            }
+        }
     }
 }
